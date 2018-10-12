4b919f2d7b4bc3848c741a9db9f1cf63d73692c6
sfzygit <461207566@qq.com>
2018-10-12 16:33:12 +0800

    rongyun demo

---
+++ src/main/java/com/bluetron/chat/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.bluetron.chat.utils;
+
+public class Constants {
+
+	public static String IM_APP_KEY= "pkfcgjstpo628";
+	public static String IM_APP_SECRET= "9JaubsypNw";
+	public static String IM_API_HOST = "http://api.cn.ronghub.com";
+	
+	public static String IM_GET_TOKEN_URL = "http://api.cn.ronghub.com/user/getToken";
+}
